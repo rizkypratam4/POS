2ae441f5c01b4dc6ca29581871041d81e1d28e7d
Rizky <rizkypratamalvbis@gmail.com>
2025-01-17 22:04:29 +0700

    create action delete, getAll, getbyid, setRefreshToken for user

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -3,10 +3,12 @@ import { encrypt } from "../utils/bcrypt.js"
 import prisma from "../utils/client.js"
 import { logger } from "../utils/winston.js"
 import { compare } from "bcrypt"
-import { generateAccessToken, generateRefreshToken } from "../utils/jwt.js"
+import { generateAccessToken, generateRefreshToken, parseJwt, verifyRefreshToken } from "../utils/jwt.js"
 
 export const createUser = async (req, res) => { 
+
     const {error, value} = userValidation(req.body)
+
     if (error) {
         return res.status(400).json({
             message: error.details[0].message,
@@ -126,6 +128,118 @@ export const loginUser = async (req, res) => {
         }
     } catch (error) {
         logger.error("controllers/user.controller.js:loginUser - " + error.message)
+        return res.status(500).json({
+            message: error.message,
+            result: null
+        })
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    try {
+        const result = await prisma.users.delete({
+            where: {
+                id: Number(req.params.id)
+            }
+        })
+        result.password = "xxxxxxxxxxxxxxxxxx"
+        return res.status(200).json({
+            message: "success",
+            result
+        })
+    } catch (error) {
+        logger.error("controllers/user.controller.js:deleteUser - " + error.message)
+        return res.status(500).json({
+            message: error.message,
+            result: null
+        })
+    }
+}
+
+export const getAllUser = async (req, res) => {
+    try {
+        const result = await prisma.users.findMany({})
+        return res.status(200).json({
+            message: "success",
+            result
+        })
+    } catch (error) {
+        logger.error("controllers/user.controller.js - " + error.message)
+        return res.status(500).json({
+            message: error.message,
+            result: null
+        })
+    }
+}
+
+export const getUserById = async (req, res) => {
+    try {
+        const result = await prisma.users.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        })
+        result.password = "xxxxxxxxxxxxxxxxxx"
+        return res.status(200).json({
+            message: "success",
+            result
+        })
+    } catch (error) {
+        logger.error(
+            "controllers/user.controller.js:getUserById - " + error.message
+        )
+        return res.status(500).json({
+            message: error.message,
+            result: null
+        })
+    }
+}
+
+export const setRefreshToken = async (req, res) => {
+    try {
+        const authHeader = req.headers["authorization"]
+        const token = authHeader && authHeader.split(" ")[1]
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Verify token failed",
+                result: null
+            })
+        }
+        const verify = verifyRefreshToken(token)
+
+        if (!verify) {
+            return res.status(401).json({
+                message: "Verify token failed",
+                result: null
+            })
+        }
+
+        let data = await parseJwt(token)
+        const user = await prisma.users.findUnique({
+            where: {
+                username: data.username,
+            }
+        })
+
+        if (!user) {
+            return res.status(401).json({
+                message: "User not found",
+                result: null
+            })
+        } else {
+            user.password = "xxxxxxxxxxxxxxxxx"
+            const accessToken = generateAccessToken(user)
+            const refreshToken = generateRefreshToken(user)
+            return res.status(200).json({
+                message: "Refresh success",
+                result: user,
+                accessToken,
+                refreshToken
+            })
+        }
+    } catch (error) {
+        logger.error("controllers/user.controller.js:setRefreshToken - " + error.message)
         return res.status(500).json({
             message: error.message,
             result: null
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,10 +1,14 @@
 import { Router } from "express"
-import { createUser, loginUser, updateUser } from "../controllers/user.controller.js"
+import { createUser, deleteUser, getAllUser, getUserById, loginUser, setRefreshToken, updateUser } from "../controllers/user.controller.js"
 import { authenticate } from "../controllers/error.controller.js"
 const userRouter = Router()
 
 userRouter.post("/users", createUser)
 userRouter.put("/users/:id", authenticate, updateUser)
 userRouter.post("/users/login", loginUser)
+userRouter.delete("/users/:id", authenticate, deleteUser)
+userRouter.get("/users", authenticate, getAllUser)
+userRouter.get("/users/refresh", authenticate, setRefreshToken)
+userRouter.get("/users/:id", authenticate, getUserById)
 
 export default userRouter
